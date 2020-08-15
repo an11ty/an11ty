66ffa64b26b6ec0fb6d1d299f7c646e88faec7b2
Tobias Davis <accounts@davistobias.com>
2020-08-15 12:00:14 -0500

    trying a different resolver technique

--- test.sh
+++ test.sh
@@ -3,6 +3,14 @@
 # Make all the installed CLI tools available.
 # PATH=$PATH:./node_modules/.bin
 
+WHEREISTHIS=`echo "console.log(require.resolve('a11ty'))" | node`
+
+echo $WHEREISTHIS
+
+WHEREISTHIS=`echo "console.log(require.resolve('ncp'))" | node`
+
+echo $WHEREISTHIS
+
 pwd
 
 ls -l ./node_modules/.bin
